816432e0cc3fd0b458995beff44da56ec026f909
unknown <Administrador@UNS07DT098909.animaedu.intranet>
2023-04-12 21:34:35 -0300

    Add class DVD.java and Pessoa.java

--- DVD.java
+++ DVD.java
@@ -5,10 +5,16 @@ package Principal.Principal;
 public class DVD {
     private String titulo;
     private String sinopse; 
+    private Genero genero;
+    private Pessoa artistaPrincipal;
+    private Pessoa diretor;
 
-    public DVD(String titulo, String sinopse) {
+    public DVD(String titulo, String sinopse, Genero genero, Pessoa artistaPrincipal, Pessoa diretor) {
         this.titulo = titulo;
         this.sinopse = sinopse;
+        this.genero = genero;
+        this.artistaPrincipal = artistaPrincipal;
+        this.diretor = diretor;
     }
 
     public String getTitulo() {
@@ -26,5 +32,30 @@ public class DVD {
     public void setSinopse(String sinopse) {
         this.sinopse = sinopse;
     }
-    
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    public Pessoa getArtistaPrincipal() {
+        return artistaPrincipal;
+    }
+
+    public void setArtistaPrincipal(Pessoa artistaPrincipal) {
+        this.artistaPrincipal = artistaPrincipal;
+    }
+
+    public Pessoa getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(Pessoa diretor) {
+        this.diretor = diretor;
+    }
+
+  
 }
